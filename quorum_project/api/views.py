81632709d94da6a5d2edf6985a45b88a6b8c4ce4
Kaê Uchôa <kae.uchoa.s@gmail.com>
2024-01-02 17:09:53 -0300

    feat: add viesets

--- quorum_project/api/views.py
+++ quorum_project/api/views.py
@@ -3,4 +3,27 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models.person.person import Person
+from .models.bill.bill import Bill
+from .models.legislator.legislator import Legislator
+from .models.vote.vote import Vote
+from .models.vote_result.vote_result import VoteResult
+from .serializers import BillSerializer, LegislatorSerializer, VoteSerializer, VoteResultSerializer
+
+# api/views.py
+class BillViewSet(viewsets.ModelViewSet):
+    queryset = Bill.objects.all()
+    serializer_class = BillSerializer
+
+class LegislatorViewSet(viewsets.ModelViewSet):
+    queryset = Legislator.objects.all()
+    serializer_class = LegislatorSerializer
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+class VoteResultViewSet(viewsets.ModelViewSet):
+    queryset = VoteResult.objects.all()
+    serializer_class = VoteResultSerializer
